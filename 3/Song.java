a4088a1fd61546c29b17242d0573e6d410f780b0
Carolineiiiii5 <43112367+Carolineiiiii5@users.noreply.github.com>
2018-12-08 23:51:12 +0800

    Create Song.java

---
+++ 3/Song.java
@@ -0,0 +1,17 @@
+package ktv;
+
+public class Song {
+	//歌曲名字
+	String name;
+	//演唱歌手
+	String singer;
+	//歌曲时长
+	String time;
+	Song(String name , String singer , String time )
+	{
+		this.name = name;
+		this.singer = singer;
+		this.time = time;
+	}
+
+}
